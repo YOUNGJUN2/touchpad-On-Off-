b626b091a5071bfefff908087ba6af45b43061fb
YJUNY <36507979+YOUNGJUN2@users.noreply.github.com>
2019-05-25 21:34:15 +0900

    Create padOn.py

---
+++ padOn.py
@@ -0,0 +1,9 @@
+import os, re, subprocess
+
+result = subprocess.check_output('xinput', shell=True)
+result = str(result)
+p = re.search('Touchpad\D*(\d+)', result)
+
+id = p.group(1)
+
+os.system('xinput set-int-prop %s "Device Enabled" 8 0' % (id))
